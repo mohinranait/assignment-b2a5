78d419299583deccb8eb8074876d3ebe1dc5fecd
Md. Ebrahim <ebrahimit49@gmail.com>
2026-07-28 21:19:47 +0600

    Designed register form

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import { RegisterForm } from "./_components/RegisterForm";
 
-const RegisterPage = () => {
+
+
+export default function RegisterPage() {
   return (
-    <div>RegisterPage</div>
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <RegisterForm />
+      </div>
+    </div>
   )
 }
-
-export default RegisterPage
